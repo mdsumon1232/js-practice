8eea58a18b3b0c32f9ef6f9add3defb1a0f5657e
Md.Sumon <sumonahamed5566@gmail.com>
2026-04-15 16:28:16 +0600

    practice 16

---
+++ practice16.js
@@ -0,0 +1,8 @@
+const num = parseFloat(prompt("Enter the number"));
+
+
+if(Math.abs(100 - num >=20) || Math.abs(400 - num >= 20)){
+    console.log("condition match");
+}else{
+    console.log("condition not match");
+}
